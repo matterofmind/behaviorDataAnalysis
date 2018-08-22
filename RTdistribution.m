aef973394da97d2481d1a3d95c6abf575d7902cd
matterofmind <41981904+matterofmind@users.noreply.github.com>
2018-08-22 23:09:47 +0900

    Update RTdistribution.m

--- RTdistribution.m
+++ RTdistribution.m
@@ -1,12 +1,13 @@
 %%% RTdistribution.m
 % to inspect the RT distribution with eyes
 % coded by Jane
-% last edit: 20180801 Wed
+% last edit: 20180822 Wed
 
 %% =======================================================================
 % %                 DATA INSPECTION : RT distribution
 % ========================================================================
-numBin = 10;
+numBin = 12;    %binning for RT distribution
+whatSD = 2.5;   %specify SD range
 for iFig = 1:2
     figure;
     for iSub = 1:length(Custom.subNames)
@@ -18,10 +19,11 @@ for iFig = 1:2
         Data(iSub).stat.RT2(2) = std(Data(iSub).RT2);   %std
         %Data(iSub).stat.RT2(3) = isoutlier(Data(iSub).RT2,'mean'); %3std
         %MATLAB 2018
+        % shortcut
         RT1mean=Data(iSub).stat.RT1(1);
         RT2mean=Data(iSub).stat.RT2(1);
-        RT1std25=2.5*Data(iSub).stat.RT1(2);
-        RT2std25=2.5*Data(iSub).stat.RT2(2);
+        RT1std=whatSD*Data(iSub).stat.RT1(2);
+        RT2std=whatSD*Data(iSub).stat.RT2(2);
         
         switch iFig
             case 1
@@ -32,9 +34,12 @@ for iFig = 1:2
                 line([RT1mean RT1mean],ylim,'Color','b');
                 line([0 0],ylim,'Color','black');
                 line([4 4],ylim,'Color','black');
-                line([RT1mean-RT1std25 RT1mean-RT1std25],ylim,'Color','r');
-                line([RT1mean+RT1std25 RT1mean+RT1std25],ylim,'Color','r');
-                title(sprintf('RT1 distribution Subj%d',iSub));
+                line(RT1mean-RT1std*[1 1],ylim,'Color','r');
+                line(RT1mean+RT1std*[1 1],ylim,'Color','r');
+                title(strcat('RT1 distribution ',Custom.subNames{iSub}));                
+                axis([-2 5 0 250]);
+                xlabel('RT1');
+                ylabel('Frequency');
             case 2
                 %RT2
                 subplot(5,5,iSub);
@@ -43,9 +48,28 @@ for iFig = 1:2
                 line([RT2mean RT2mean],ylim,'Color','b');
                 line([0 0],ylim,'Color','black');
                 line([4 4],ylim,'Color','black');
-                line([RT2mean-RT2std25 RT2mean-RT2std25],ylim,'Color','r');
-                line([RT2mean+RT2std25 RT2mean+RT2std25],ylim,'Color','r');
-                title(sprintf('RT2 distribution Subj%d',iSub));
+                line(RT2mean-RT2std*[1 1],ylim,'Color','r');
+                line(RT2mean+RT2std*[1 1],ylim,'Color','r');
+                title(strcat('RT2 distribution ',Custom.subNames{iSub}))
+                axis([-2 5 0 250]);
+                xlabel('RT2');
+                ylabel('Frequency');
         end
-    end
-end
+        % inlier =1 , outlier = 0
+        Data(iSub).InOut{1}= [(Data(iSub).RT1>(RT1mean-RT1std))&(Data(iSub).RT1<(RT1mean+RT1std))];
+        Data(iSub).InOut{2}= [(Data(iSub).RT2>RT2mean-RT2std)&(Data(iSub).RT2<RT2mean+RT2std)];
+        % count how many outliers are there
+        Data(iSub).howManyOut(1) = length(Data(iSub).RT1)-nnz(Data(iSub).InOut{1});
+        Data(iSub).howManyOut(2) = length(Data(iSub).RT2)-nnz(Data(iSub).InOut{2});
+        
+        % filter only those outlier and incorrect = 1
+        Data(iSub).OutIncorrect{1}= [(Data(iSub).InOut{1}==0)&(Data(iSub).Acc==0)];
+        Data(iSub).OutIncorrect{2}= [(Data(iSub).InOut{2}==0)&(Data(iSub).Acc==0)];
+        % count how many outlier & incorrect are there
+        Data(iSub).howManyIncorrectOut(1) = nnz(Data(iSub).OutIncorrect{1});
+        Data(iSub).howManyIncorrectOut(2) = nnz(Data(iSub).OutIncorrect{2});
+        % count how many outlier & correct are there
+        Data(iSub).howManyCorrectOut(1) = Data(iSub).howManyOut(1)-nnz(Data(iSub).OutIncorrect{1});
+        Data(iSub).howManyCorrectOut(2) = Data(iSub).howManyOut(2)-nnz(Data(iSub).OutIncorrect{2});
+    end % ... subject loop
+end %... fig
